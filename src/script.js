f03eb116e840fefe5c8a3cab87c5297cb60ce27a
Sad Boy <berijani.a2006@gmail.com>
2024-08-12 15:24:00 +0330

    update

---
+++ src/script.js
@@ -0,0 +1,16 @@
+var toggleOpen = document.getElementById('toggleOpen');
+var toggleClose = document.getElementById('toggleClose');
+var collapseMenu = document.getElementById('collapseMenu');
+
+function handleClick() {
+  if (collapseMenu.style.display === 'block') {
+    collapseMenu.style.display = 'none';
+  } else {
+    collapseMenu.style.display = 'block';
+  }
+}
+
+toggleOpen.addEventListener('click', handleClick);
+toggleClose.addEventListener('click', handleClick);
+
+

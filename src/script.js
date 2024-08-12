245782e28630c2bb79eff1c9d498191780ad47a4
Sad Boy <berijani.a2006@gmail.com>
2024-08-12 18:06:39 +0330

    v1 main ready

--- src/script.js
+++ src/script.js
@@ -14,3 +14,36 @@ toggleOpen.addEventListener('click', handleClick);
 toggleClose.addEventListener('click', handleClick);
 
 
+
+/* =========== Toggle Menu ============ */
+const openNav = document.querySelector(".open-btn");
+const closeNav = document.querySelector(".close-btn");
+const menu = document.querySelector(".nav-list");
+
+openNav.addEventListener("click", () => {
+  menu.classList.add("show");
+});
+
+closeNav.addEventListener("click", () => {
+  menu.classList.remove("show");
+});
+
+
+/**slider */
+
+// $('#dasteh-bandi').mouseenter(function (){
+//   $('.master-div').fadeIn(100)
+// })
+// $('#dasteh-bandi').mouseleave(function (){
+//   $('.master-div').fadeOut(100)
+// })
+
+
+("#left-img-story").click(function (){
+  $('.story-ul').animate({'margin-left':'+=100px'})
+})
+$("#right-img-story").click(function (){
+  $('.story-ul').animate({'margin-right':'-=100px'})
+
+})
+
